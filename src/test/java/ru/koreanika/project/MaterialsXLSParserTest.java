156b2059c22fbda15a4eae3ea913a1f390d41ddc
Konstantin Sokolov <vtqveant@gmail.com>
2023-10-24 13:16:52 +0300

    listing materials without images in a test

--- src/test/java/ru/koreanika/project/MaterialsXLSParserTest.java
+++ src/test/java/ru/koreanika/project/MaterialsXLSParserTest.java
@@ -2,16 +2,19 @@ package ru.koreanika.project;
 
 import org.junit.Ignore;
 import org.junit.Test;
+import ru.koreanika.Common.Material.ImageIndex;
 import ru.koreanika.Common.Material.Material;
 import ru.koreanika.Common.PlumbingElementForSale.PlumbingElement;
 import ru.koreanika.Common.PlumbingElementForSale.PlumbingType;
+import ru.koreanika.service.ServiceLocator;
 
+import java.io.IOException;
 import java.util.*;
 
 public class MaterialsXLSParserTest {
 
-    public static final String MATERIALS_LIST_PATH = "/home/transcend/code/KOREANIKA/DATA/materials_1_2004.xls";
-    public static final String ANALOGS_LIST_PATH = "material_analogs.xls";
+    public static final String MATERIALS_XLS_PATH = "materials_1_2004.xls";
+    public static final String ANALOGS_XLS_PATH = "material_analogs.xls";
 
     @Test
     @Ignore
@@ -21,9 +24,32 @@ public class MaterialsXLSParserTest {
         Set<PlumbingType> availablePlumbingTypes = new LinkedHashSet<>();
         Map<String, Double> materialsDeliveryFromManufacture = new HashMap<>();
 
-        FacadeXLSParser parser = new FacadeXLSParser(MATERIALS_LIST_PATH, ANALOGS_LIST_PATH);
+        FacadeXLSParser parser = new FacadeXLSParser(MATERIALS_XLS_PATH, ANALOGS_XLS_PATH);
         parser.populateLists(materialsListAvailable, plumbingElementsList, availablePlumbingTypes, materialsDeliveryFromManufacture);
         System.out.println(materialsListAvailable.size());
+    }
+
+    @Ignore
+    @Test
+    public void matchMaterialsWithImagesFromImageIndex() throws IOException {
+        List<Material> materialsListAvailable = new ArrayList<>();
+        List<PlumbingElement> plumbingElementsList = new ArrayList<>();
+        Set<PlumbingType> availablePlumbingTypes = new LinkedHashSet<>();
+        Map<String, Double> materialsDeliveryFromManufacturer = new HashMap<>();
+
+        MaterialsXLSParser materialsXLSParser = new MaterialsXLSParser(MATERIALS_XLS_PATH);
+        materialsXLSParser.populateLists(materialsListAvailable, plumbingElementsList, availablePlumbingTypes,
+                materialsDeliveryFromManufacturer);
+
+        ImageIndex imageIndex = ServiceLocator.getService("ImageIndex", ImageIndex.class);
+
+        System.out.println("id;name");
+        for (Material material : materialsListAvailable) {
+            String id = material.getId();
+            if (!imageIndex.containsKey(id)) {
+                System.out.println(id + ";\"" + material.getName() + "\"");
+            }
+        }
     }
 
 }
